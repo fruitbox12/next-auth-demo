4b20c12cfb6b80a7a7116771663758c49207c6c5
fruitbox12 <dylanwong007@gmail.com>
2023-06-06 01:26:23 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = {	reactStrictMode: false,
+	typescript: {
+	  ignoreBuildErrors: true,
+	},
+	eslint: {
+        ignoreDuringBuilds: true,
+    },
+  };
+  
   experimental: {
     appDir: true
   },
